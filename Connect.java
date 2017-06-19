d2c794c3659599a464352e6b4aa1ebf15ab425f9
Alex <agardner210@gmail.com>
2017-06-19 16:39:17 -0500

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -1,12 +1,18 @@
 public class Connect
 {
+	private String name = null;
+	private String server = null;
+	private int port = 6667;
+	
 	public Connect()
 	{
 		
 	}
-	public Connect(String network, String channel, int port)
+	public Connect(String network, String uname, int uport)
 	{
-		
+		name = uname;
+		network = server;
+		port = uport;
 	}
 	void login(String network, int port)
 	{
